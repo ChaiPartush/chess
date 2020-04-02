a087e970903d61fa23bd29ac8c0da360b1e4fb63
Chai Partush <chai1996@gmail.com>
2020-04-02 18:07:09 +0300

    Add pieces object

---
+++ pieces.js
@@ -0,0 +1,14 @@
+var Pieces = {
+    WHITE_KING: '\u{2654}',
+    BLACK_KING: '\u{265A}',
+    WHITE_QUEEN: '\u{2655}',
+    BLACK_QUEEN: '\u{265B}',
+    WHITE_ROOK: '\u{2656}',
+    BLACK_ROOK: '\u{265C}',
+    WHITE_KNIGHT: '\u{2658}',
+    BLACK_KNIGHT: '\u{265E}',
+    WHITE_BISHOP: '\u{2657}',
+    BLACK_BISHOP: '\u{265D}',
+    WHITE_PAWN: '\u{2659}',
+    BLACK_PAWN: '\u{265F}'
+};
