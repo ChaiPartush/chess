a2f707f1f06bdce2fe5d18a8396d11baafc7a621
Chai Partush <chai1996@gmail.com>
2020-04-22 18:10:13 +0300

    Change consts naming

--- consts.js
+++ consts.js
@@ -20,39 +20,21 @@ var secondRowUserBoard = {
 }
 
 const AllPieces = {
-    WHITE_KING: { color: ColorsPieces.WHITE, piece: FirstRowOfUserBoard.KING },
-    BLACK_KING: { color: ColorsPieces.BLACK, piece: FirstRowOfUserBoard.KING },
-    WHITE_QUEEN: { color: ColorsPieces.WHITE, piece: FirstRowOfUserBoard.QUEEN },
-    BLACK_QUEEN: { color: ColorsPieces.BLACK, piece: FirstRowOfUserBoard.QUEEN },
-    WHITE_ROOK: { color: ColorsPieces.WHITE, piece: FirstRowOfUserBoard.ROOK },
-    BLACK_ROOK: { color: ColorsPieces.BLACK, piece: FirstRowOfUserBoard.ROOK },
-    WHITE_KNIGHT: { color: ColorsPieces.WHITE, piece: FirstRowOfUserBoard.KNIGHT },
-    BLACK_KNIGHT: { color: ColorsPieces.BLACK, piece: FirstRowOfUserBoard.KNIGHT },
-    WHITE_BISHOP: { color: ColorsPieces.WHITE, piece: FirstRowOfUserBoard.BISHOP },
-    BLACK_BISHOP: { color: ColorsPieces.BLACK, piece: FirstRowOfUserBoard.BISHOP },
-    WHITE_PAWN: { color: ColorsPieces.WHITE, piece: secondRowUserBoard.PAWN },
-    BLACK_PAWN: { color: ColorsPieces.BLACK, piece: secondRowUserBoard.PAWN },
+    ROOK: 'Rook',
+    KNIGHT: 'knight',
+    BISHOP: 'bishop',
+    QUEEN: 'queen',
+    KING: 'king',
+    PAWN: 'pawn',
     Empty: 'empty'
 }
+
 
 const FIRST_BLACK_PAWN_ROW_POSITION = 1;
 const FIRST_WHITE_PAWN_ROW_POSITION = 6;
 
 
-// var UnicodePieces = {
-//     WHITE_KING: '\u{2654}',
-//     BLACK_KING: '\u{265A}',
-//     WHITE_QUEEN: '\u{2655}',
-//     BLACK_QUEEN: '\u{265B}',
-//     WHITE_ROOK: '\u{2656}',
-//     BLACK_ROOK: '\u{265C}',
-//     WHITE_KNIGHT: '\u{2658}',
-//     BLACK_KNIGHT: '\u{265E}',
-//     WHITE_BISHOP: '\u{2657}',
-//     BLACK_BISHOP: '\u{265D}',
-//     WHITE_PAWN: '\u{2659}',
-//     BLACK_PAWN: '\u{265F}'
-// };
+
 
 
 
